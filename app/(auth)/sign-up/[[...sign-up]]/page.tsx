be91b610788da9d443288b123a3a88cfa3f7d38e
taduchuy04@gmail.com <taduchuy04@gmail.com>
2024-05-20 16:04:21 -0400

    Done Authentication

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,39 @@
+import { SignUp, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
+
+export default function Page() {
+    return (
+        <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+
+            {/* Grid column 1 */}
+            <div className="h-full lg:flex flex-col items-center justify-center px4">
+
+                {/* Text div */}
+                <div className="text-center space-y-4 pt-16">
+                    <h1 className="font-bold text-3xl text-[#2E2A47]">
+                        Welcome back!
+                    </h1>
+                    <p className="text-base text-[#7E8CA0]">
+                        Log in or Create an account to continue
+                    </p>
+                </div>
+
+                {/* Log in / Sign up div */}
+                <div className="flex items-center justify-center mt-8">
+                    <ClerkLoaded>
+                        <SignUp path="/sign-up" />
+                    </ClerkLoaded>
+                    <ClerkLoading>
+                        <Loader2 className="animate-spin text-muted-foreground" />
+                    </ClerkLoading>
+                </div>
+            </div>
+
+            {/* Grid column 2, hidden on mobile */}
+            <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
+                <Image src = "/logo.svg" height={100} width={100} alt="Logo"/>
+            </div>
+        </div>
+    );
+}
